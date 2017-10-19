9e79aabb120a587afeb60209f8b137c358b0bcd4
tocurd <tocurd@qq.com>
2017-10-19 18:00:00 +0800

    update

--- template/admin/template/header.php
+++ template/admin/template/header.php
@@ -3,3 +3,5 @@
 <head>
 	<meta charset="UTF-8">
 	<title>{defaultTitle}</title>
+	<link rel="stylesheet" href="./assets/css/admin/style.css">
+	<link rel="stylesheet" href="//at.alicdn.com/t/font_446976_yzlwyhdfumghw7b9.css">
--- template/admin/user/login.php
+++ template/admin/user/login.php
@@ -1,16 +1,22 @@
-
+	<link rel="stylesheet" href="./assets/css/admin/login.css">
 </head>
 <body>
-	
+
 	<div class="login" api-name="Admin_user/Login">
-		<input type="text" api-param-name="username" name="用户名" max="16" min="5" placeholder="请输入您的管理员账号">
-		<input type="password" api-param-name="password" name="密码" max="16" min="5" placeholder="请输入您的管理员密码">
+
+		<div class="item">
+			<span><i class="iconfont icon-usered"></i></span>
+			<input type="text" api-param-name="username" name="用户名" max="16" min="5" placeholder="请输入您的管理员账号">
+		</div>
+
+		<div class="item">
+			<span><i class="iconfont icon-mima4"></i></span>
+			<input type="password" api-param-name="password" name="密码" max="16" min="5" placeholder="请输入您的管理员密码">
+		</div>
+
 		<button class="btn" api-event="submit">登录账号</button>
 	</div>
 
 	<?php $this->load->view(ADMIN_TEMPLATE . '/template/footer');?>
-	<script type="text/javascript">
-		
-	</script>
 </body>
 </html>
