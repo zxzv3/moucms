4559f3f01456147c1878004c60f6692bb5940a91
tocurd <tocurd@qq.com>
2017-10-20 17:47:43 +0800

    update

--- template/admin/template/header.php
+++ template/admin/template/header.php
@@ -4,4 +4,4 @@
 	<meta charset="UTF-8">
 	<title>{defaultTitle}</title>
 	<link rel="stylesheet" href="./assets/css/admin/style.css">
-	<link rel="stylesheet" href="//at.alicdn.com/t/font_446976_yzlwyhdfumghw7b9.css">
+	<link rel="stylesheet" href="//at.alicdn.com/t/font_446976_jkt059g1indhd7vi.css">
--- template/admin/user/login.php
+++ template/admin/user/login.php
@@ -1,22 +1,213 @@
 	<link rel="stylesheet" href="./assets/css/admin/login.css">
 </head>
 <body>
+	<div class="container">
+		<div id="particles-js"></div>
+	</div>
 
 	<div class="login" api-name="Admin_user/Login">
-
-		<div class="item">
-			<span><i class="iconfont icon-usered"></i></span>
-			<input type="text" api-param-name="username" name="用户名" max="16" min="5" placeholder="请输入您的管理员账号">
+		<div class="top-header">
+			<h1>moucms</h1> <br>
+			<span>更高效更便捷的快速开发系统</span>
 		</div>
+		<div class="box">
+			<div class="item">
+				<span><i class="iconfont icon-usered"></i></span>
+				<input type="text" api-param-name="username" name="用户名" max="16" min="5" placeholder="请输入您的管理员账号">
+			</div>
 
-		<div class="item">
-			<span><i class="iconfont icon-mima4"></i></span>
-			<input type="password" api-param-name="password" name="密码" max="16" min="5" placeholder="请输入您的管理员密码">
+			<div class="item">
+				<span><i class="iconfont icon-mima4"></i></span>
+				<input type="password" api-param-name="password" name="密码" max="16" min="5" placeholder="请输入您的管理员密码">
+			</div>
+
+			<div class="item verification">
+				<span><i class="iconfont icon-yanzhengma1" style=""></i></span>
+				<input type="password" api-param-name="password" name="密码" max="16" min="5" placeholder="请输入右侧的验证码">
+				<img src="http://www.hfls122.com:88/verification/verification/code" alt="">
+			</div>
+			<button class="btn fezocms" api-event="submit">登录账号</button>
 		</div>
-
-		<button class="btn" api-event="submit">登录账号</button>
 	</div>
 
 	<?php $this->load->view(ADMIN_TEMPLATE . '/template/footer');?>
+
+	<script src="./assets/js/three.js"></script>
+
+	<script src="./assets/js/Projector.js"></script>
+	<script src="./assets/js/CanvasRenderer.js"></script>
+
+	<script>
+
+		var SEPARATION = 100, AMOUNTX = 50, AMOUNTY = 50;
+
+		var container, stats;
+		var camera, scene, renderer;
+
+		var particles, particle, count = 0;
+
+		var mouseX = 0, mouseY = -500;
+
+		var windowHalfX = window.innerWidth / 2;
+		var windowHalfY = window.innerHeight / 2;
+
+		init();
+		animate();
+
+		function init() {
+
+			container = document.createElement( 'div' );
+			container.style.position = 'relative';
+			container.style.top = '200px';
+			document.body.appendChild( container );
+
+			camera = new THREE.PerspectiveCamera( 55, window.innerWidth / window.innerHeight, 1, 10000 );
+			camera.position.z = 1400;
+
+			scene = new THREE.Scene();
+
+			particles = new Array();
+
+			var PI2 = Math.PI * 2;
+			var material = new THREE.SpriteCanvasMaterial( {
+
+				color: 0xdddddd,
+				program: function ( context ) {
+
+					context.beginPath();
+					context.arc( 0, 0, 0.5, 0, PI2, true );
+					context.fill();
+
+				}
+
+			} );
+
+			var i = 0;
+
+			for ( var ix = 0; ix < AMOUNTX; ix ++ ) {
+
+				for ( var iy = 0; iy < AMOUNTY; iy ++ ) {
+
+					particle = particles[ i ++ ] = new THREE.Sprite( material );
+					particle.position.x = ix * SEPARATION - ( ( AMOUNTX * SEPARATION ) / 2 );
+					particle.position.z = iy * SEPARATION - ( ( AMOUNTY * SEPARATION ) / 2 );
+					scene.add( particle );
+
+				}
+
+			}
+
+			renderer = new THREE.CanvasRenderer({
+				alpha:true,
+				antialias: true
+
+			});
+			renderer.setPixelRatio( window.devicePixelRatio );
+			renderer.setSize( window.innerWidth, window.innerHeight );
+			container.appendChild( renderer.domElement );
+			renderer.setClearColor(0xFFFFFF, 0.0); 
+
+
+
+			/*stats = new Stats();
+			container.appendChild( stats.dom );*/
+
+			document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+			document.addEventListener( 'touchstart', onDocumentTouchStart, false );
+			document.addEventListener( 'touchmove', onDocumentTouchMove, false );
+
+			//
+
+			window.addEventListener( 'resize', onWindowResize, false );
+
+		}
+
+		function onWindowResize() {
+
+			windowHalfX = window.innerWidth / 2;
+			windowHalfY = window.innerHeight / 2;
+
+			camera.aspect = window.innerWidth / window.innerHeight;
+			camera.updateProjectionMatrix();
+
+			renderer.setSize( window.innerWidth, window.innerHeight );
+
+		}
+
+		//
+
+		function onDocumentMouseMove( event ) {
+
+			//mouseX = event.clientX - windowHalfX;
+			//mouseY = event.clientY - windowHalfY;
+
+		}
+
+		function onDocumentTouchStart( event ) {
+
+			if ( event.touches.length === 1 ) {
+
+				event.preventDefault();
+
+				mouseX = event.touches[ 0 ].pageX - windowHalfX;
+				mouseY = event.touches[ 0 ].pageY - windowHalfY - 200;
+
+			}
+
+		}
+
+		function onDocumentTouchMove( event ) {
+
+			if ( event.touches.length === 1 ) {
+
+				event.preventDefault();
+
+				mouseX = event.touches[ 0 ].pageX - windowHalfX;
+				mouseY = event.touches[ 0 ].pageY - windowHalfY;
+
+			}
+
+		}
+
+		//
+
+		function animate() {
+
+			requestAnimationFrame( animate );
+
+			render();
+			//stats.update();
+
+		}
+
+		function render() {
+
+			camera.position.x += ( mouseX - camera.position.x ) * .05;
+			camera.position.y += ( - mouseY - camera.position.y ) * .05;
+			camera.lookAt( scene.position );
+
+			var i = 0;
+
+			for ( var ix = 0; ix < AMOUNTX; ix ++ ) {
+
+				for ( var iy = 0; iy < AMOUNTY; iy ++ ) {
+
+					particle = particles[ i++ ];
+					particle.position.y = ( Math.sin( ( ix + count ) * 0.3 ) * 50 ) +
+						( Math.sin( ( iy + count ) * 0.5 ) * 50 );
+					particle.scale.x = particle.scale.y = ( Math.sin( ( ix + count ) * 0.3 ) + 1 ) * 4 +
+						( Math.sin( ( iy + count ) * 0.5 ) + 1 ) * 4;
+
+				}
+
+			}
+
+			renderer.render( scene, camera );
+
+			count += 0.05;
+
+		}
+
+	</script>
 </body>
 </html>
